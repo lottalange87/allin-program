d5c47a24a2fe8e9624bfb24b9bb36b01c9efc395
Jörg <>
2026-02-18 08:40:08 +0100

    fix: extend wave svg with extra rect to cover iOS gap

--- app/page.tsx
+++ app/page.tsx
@@ -294,8 +294,9 @@ export default function Home() {
         </div>
 
         {/* Bottom wave */}
-        <div className="absolute bottom-0 left-0 right-0 leading-none">
-          <svg className="block" viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="absolute left-0 right-0 leading-none translate-y-px">
+          <svg className="block w-full h-auto" viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+            <rect x="0" y="118" width="1440" height="4" fill="#f9fafb" />
             <path
               d="M0 120L60 105C120 90 240 60 360 45C480 30 600 30 720 37.5C840 45 960 60 1080 67.5C1200 75 1320 75 1380 75L1440 75V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0Z"
               fill="#f9fafb"
